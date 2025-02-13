a444c9eef5620011f74ba6035bc18fa2b0f0a8e9
dezso <serban.dezso.david.97.07.08@gmail.com>
2025-02-13 20:42:47 +0100

    Összes esemény betöltése a FakeAPI-ból

--- src/app/all-events/all-events.component.ts
+++ src/app/all-events/all-events.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { BaseService } from '../base.service';
 
 @Component({
   selector: 'app-all-events',
@@ -9,17 +10,28 @@ import { AuthService } from '../auth.service';
 })
 export class AllEventsComponent  {
   clickedEventDetails:any={};
-  url = "./jsonFiles/allEvents.json";
+  // url = "./jsonFiles/allEvents.json";
 
   //user tárolása
   user:any
+  dataFromApi:any
 
 
 
-  constructor(private httpClient:HttpClient, private auth:AuthService ) {
+  constructor(private httpClient:HttpClient, private auth:AuthService, private base:BaseService ) {
     // user lecsekkolása
     this.auth.getLoggedUser().subscribe(
       (u)=>this.user=u
+    )
+
+    this.getDataFromApi()
+  }
+
+  getDataFromApi(){
+    this.base.adatSub.subscribe(
+      (res:any) => {
+        this.dataFromApi = res
+      }
     )
   }
 
