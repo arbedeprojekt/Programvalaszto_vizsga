cb35a65436d67c45b2e946dcf146162110e73971
szbeata <szluka.beata@gmail.com>
2025-04-06 23:27:06 +0200

    hambuger menu collapse

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { LocalStorageService } from '../local-storage.service';
+import { Collapse } from 'bootstrap';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
+
+
 export class NavbarComponent {
 
   // Ebben tárolom el a user adatait, és ezen keresztül jelzem a felhasználónak
@@ -30,6 +33,15 @@ export class NavbarComponent {
   logoutUserWithLaravel() {
     this.auth.logoutUserFromLaravel()
     //console.log("A felhasználó sikeresen kijelentkezve.")
+  }
+
+  //hamburger menü becsukódjon
+  collapseNavbar() {
+    const el = document.getElementById('navbarSupportedContent');
+    if (el?.classList.contains('show')) {
+      const collapse = Collapse.getOrCreateInstance(el);
+      collapse.hide();
+    }
   }
 
 }
